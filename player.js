000c93f548018ac8b6336f1797269b836ec8c46e
emmacintosh <emmammrussell@gmail.com>
2022-06-12 19:07:36 -0600

    Add functionality for the expanded game mode

--- player.js
+++ player.js
@@ -1,12 +1,18 @@
 class Player {
   constructor() {
+    this.name = "";
     this.wins = 0;
     this.rock = false;
     this.paper = false;
     this.scissors = false;
+    this.attack = false;
+    this.jumpAttack = false;
+    this.lowAttack = false;
+    this.highBlock = false;
+    this.lowBlock = false;
 
   }
-  takeTurn(id){
+  takeTurnTraining(id){
     if (id === 'rockButton') {
       this.rock = true;
     }
@@ -17,9 +23,33 @@ class Player {
       this.scissors = true;
     }
   }
+
+  takeTurnTournament(id){
+    if (id === 'atkButton') {
+      this.attack = true;
+    }
+    else if (id === 'jumpAtkButton')  {
+      this.jumpAttack = true;
+    }
+    else if (id === 'lowAtkButton')  {
+      this.lowAttack = true;
+    }
+    else if (id === 'highBlockButton')  {
+      this.highBlock = true;
+    }
+    else if (id === 'lowBlockButton')  {
+      this.lowBlock = true;
+    }
+  }
+
   resetScores(){
     this.rock = false;
     this.paper = false;
     this.scissors = false;
+    this.attack = false;
+    this.jumpAttack = false;
+    this.lowAttack = false;
+    this.highBlock = false;
+    this.lowBlock = false;
   }
 }
